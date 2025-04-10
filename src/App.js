3ce3a192c6436b23b776400b8a85f88d04363748
Mojtaba Fouladi <sepid.rasouli@outlook.com>
2025-04-10 11:28:49 +0200

    ch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [status, setStatus] = useState('checking...');
 
   useEffect(() => {
-    axios.get('https://app.mojtabaonline.me:3000/check-db')
+    axios.get('https://app.mojtabaonline.me/check-db')
       .then(res => {
         setStatus(`✅ MongoDB is ${res.data.status}`);
       })
